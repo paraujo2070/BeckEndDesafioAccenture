f525cd36c22ac9f92be0627f8c41ec9efd646c32
Pedro Antonio <paraujo2070@hotmail.com>
2020-10-04 17:28:05 -0300

    Rota translate

--- BeckEnd/functions/index.js
+++ BeckEnd/functions/index.js
@@ -1,6 +1,26 @@
 const functions = require('firebase-functions');
 
- exports.helloWorld = functions.https.onRequest((request, response) => {   
-    functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+ exports.translate = functions.https.onRequest((request, response) => {   
+  const text = request.body.texto;
+  const str =
+      [
+          ['a', 'b', 'c'],
+          ['d', 'e', 'f'],
+          ['g', 'h', 'i'],
+          ['j', 'k', 'l'],
+          ['m', 'n', 'o'],
+          ['p', 'q', 'r', 's'],
+          ['t', 'u', 'v'],
+          ['w', 'x', 'y', 'z']
+      ];
+
+  let arrayMsg = text.split(" ");
+  let result = "";
+  for (let i = 0; i < arrayMsg.length; i++) {
+      let num = parseInt(arrayMsg[i][0]);
+      let tecla = num - 2;
+      let caracter = arrayMsg[i].length - 1;
+      result += str[tecla][caracter];
+  }
+  return response.send(result);
 });
